4e0bd5339720a0a8f2715dcc580c5690335a9518
Vulpestellar <149997226+Vulpestellar@users.noreply.github.com>
2024-11-15 17:57:19 +0800

    minor edits

--- app.js
+++ app.js
@@ -13,15 +13,12 @@ hotelApp.getRoom("PS1");
 hotelApp.addStaff("Vick Tim","Janitor");
 
 hotelApp.removeStaff(4);
-
 hotelApp.removeStaff(4)
 
 hotelApp.changeStaffRole(1,"Senior Manager")
 
 hotelApp.getAllStaff("")
-
 hotelApp.getAllStaff("Front Desk")
-
 hotelApp.getAllStaff("a")
 
 hotelApp.getStaff(2)
